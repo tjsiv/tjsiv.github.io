4e1054748e85b47cd77f22bab53fa43d79b21ed8
tjsiv <tjsivelsjr@gmail.com>
2024-04-17 20:09:28 -0700

    refactoring to cleen index added item file

---
+++ item.js
@@ -0,0 +1,30 @@
+class Item {
+    constructor({ coords, imgSrc = './img/sprites/cherry.png' }) {
+      this.coords = coords
+      this.radius = 8
+      this.image = new Image()
+      this.image.src = imgSrc
+      this.loaded = false
+      this.image.onload = () => {
+        this.loaded = true
+      }
+      this.center = JSON.parse(JSON.stringify(coords))
+      this.radians = 0
+    }
+  
+    create() {
+      if (!this.loaded) return
+  
+      c.drawImage(
+        this.image,
+        this.coords.x - this.image.width / 2,
+        this.coords.y - this.image.height / 2,
+      )
+  
+      this.radians += 0.05
+  
+      this.coords.x = this.center.x + Math.cos(this.radians)
+      this.coords.y = this.center.y + Math.sin(this.radians)
+    }
+  }
+  
